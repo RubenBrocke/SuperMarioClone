f13c1c0a0ea86ed842ba9c8bff75e24a7927434a
Danny de Jong <dannygjdejong@gmail.com>
2017-03-07 00:49:37 +0100

    Added general versions of classes to the game
    Added:
    -Tangible (not finished)
    -Moveable (not finished)
    -Mario (not finished)
    -Coin (not finished)
    -Solid (not finished)
    -Floor (not finished)

---
+++ SuperMarioClone/SuperMarioClone/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Coin : Tangible
+{
+	private int _value
+	{
+		get;
+		set;
+	}
+
+	private bool _moveable
+	{
+		get;
+		set;
+	}
+
+	public virtual void AddCoin(Mario mario)
+	{
+		throw new System.NotImplementedException();
+	}
+
+}
+
