2278ef92d02dda0ccaa377bb74607013536afbb0
MRX <sasgeweer@gmail.com>
2017-03-09 14:08:44 +0100

    Modified: Mysteryblock

--- SuperMarioClone/SuperMarioClone/MysteryBlock.cs
+++ SuperMarioClone/SuperMarioClone/MysteryBlock.cs
@@ -11,13 +11,23 @@ namespace SuperMarioClone.Content
 {
     class MysteryBlock : Solid
     {
-        public MysteryBlock(int _x, int _y, Level lvl, ContentManager cm) : base()
+        private GameObject _mysteryObject { get; set; }
+        public MysteryBlock(int _x, int _y, Level lvl, ContentManager cm, GameObject MysteryObject) : base()
         {
+            //TODO: add animation later
+            _mysteryObject = MysteryObject;
             X = _x;
             Y = _y;
             currentLevel = lvl;
             sprite = cm.Load<Texture2D>("MysteryBlock");
-            hitbox = new Rectangle(X, Y, 12, 16);
+            hitbox = new Rectangle(X, Y, 16, 16); // TODO: numbers represent pixels, change magic number
+        }
+
+
+        //TODO: Will eject whatever the mystery block contains
+        public void Eject()
+        {
+
         }
     }
 }
