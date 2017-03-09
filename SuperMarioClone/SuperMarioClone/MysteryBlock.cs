da100866a27680bf462af4398771f0681407378b
MRX <sasgeweer@gmail.com>
2017-03-09 13:49:00 +0100

    Added: MysteryBlock Class

---
+++ SuperMarioClone/SuperMarioClone/MysteryBlock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarioClone.Content
+{
+    class MysteryBlock : Solid
+    {
+        public MysteryBlock(int _x, int _y, Level lvl, ContentManager cm) : base()
+        {
+            X = _x;
+            Y = _y;
+            currentLevel = lvl;
+            sprite = cm.Load<Texture2D>("MysteryBlock");
+            hitbox = new Rectangle(X, Y, 12, 16);
+        }
+    }
+}
