5dec756314d3a68a192f4719922d531c85d4a6c0
Danny de Jong <dannygjdejong@gmail.com>
2017-03-11 23:19:18 +0100

    Added mysteryBlock

--- SuperMarioClone/SuperMarioClone/MysteryBlock.cs
+++ SuperMarioClone/SuperMarioClone/MysteryBlock.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SuperMarioClone
 {
@@ -13,6 +14,9 @@ namespace SuperMarioClone
     {
         private Type _mysteryObject { get; set; }
         private ContentManager _cm;
+        private bool _hasBeenUsed = false;
+        private int _spriteImageIndex = 0;
+        private Timer _timer;
 
         public MysteryBlock(int _x, int _y, Level lvl, ContentManager cm, Type MysteryObject) : base()
         {
@@ -22,18 +26,41 @@ namespace SuperMarioClone
             Y = _y;
             currentLevel = lvl;
             _cm = cm;
-            sprite = _cm.Load<Texture2D>("MysteryBlock");
+            sprite = _cm.Load<Texture2D>("MysteryBlockSheet");
             hitbox = new Rectangle(X, Y, 16, 16); // TODO: numbers represent pixels, change magic number
+
+            _timer = new Timer(ChangeSpriteIndex);
+            _timer.Change(0, 120);
         }
 
-
-        //TODO: Will eject whatever the mystery block contains
         public void Eject(Mario mario, float vY)
         {
-            if (vY < 0)
+            if (vY < 0 && !_hasBeenUsed)
             {
-                currentLevel.ToAddGameObject((GameObject)Activator.CreateInstance(_mysteryObject, X + 16, Y - hitbox.Height, currentLevel, _cm));
+                currentLevel.ToAddGameObject((GameObject)Activator.CreateInstance(_mysteryObject, X, Y - hitbox.Height, currentLevel, _cm));
+                _hasBeenUsed = true;
             } 
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture: sprite, position: new Vector2(X, Y), sourceRectangle: new Rectangle(16 * _spriteImageIndex, 0, 16, 16));
+        }
+
+        private void ChangeSpriteIndex(object state)
+        {
+            if (_spriteImageIndex < 3)
+            {
+                _spriteImageIndex++;
+            }
+            else
+            {
+                _spriteImageIndex = 0;
+            }
+            if (_hasBeenUsed)
+            {
+                _spriteImageIndex = 4;
+            }
         }
     }
 }
