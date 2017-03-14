b3a440793477b2fbab9740cdee4232d493bb7a0d
MRX <sasgeweer@gmail.com>
2017-03-14 11:08:40 +0100

    Changed

--- SuperMarioClone/SuperMarioClone/LevelReader.cs
+++ SuperMarioClone/SuperMarioClone/LevelReader.cs
@@ -23,7 +23,10 @@ namespace SuperMarioClone
             StreamReader lvlReader = new StreamReader(@"..\..\..\..\Level" + levelNumber + ".txt");
             string line;
             Level level = new Level();
-            int x, y, width, height; //elk een eigen regel
+            int x;
+            int y;
+            int width;
+            int height; 
 
 
             while((line = lvlReader.ReadLine()) != null)
