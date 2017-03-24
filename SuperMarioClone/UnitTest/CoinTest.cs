3785aa4719b38e01fcb166ab57c68cee6c5f068f
Danny de Jong <dannygjdejong@gmail.com>
2017-03-24 14:01:43 +0100

    Modified: UnitTests
    Cleaned up the CoinTest class and tried to add something that'd return
    an empty Texture2D to work with the animator class in the mock
    contentManager but failed...

---
+++ SuperMarioClone/UnitTest/CoinTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarioClone;
+using System.Reflection;
+using UnitTest.MockClasses;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CoinTest
+    {
+        private TestContentManager _contentManager;
+        private Level _level;
+        private Coin _coin;
+        private Mario _mario;
+
+        [TestInitialize]
+        void Init()
+        {
+            _contentManager = new TestContentManager();
+            _level = new Level();
+            _mario = new Mario(1, 1, _level, _contentManager);
+            _coin = new Coin(1, 1, _level, _contentManager);
+            _level.ToAddGameObject(_mario);
+            _level.ToAddGameObject(_coin);
+            _level.AddGameObjects();
+        }
+
+        [TestMethod]
+        public void Coin_AddCoin()
+        {
+            Init();
+            _coin.AddCoin(_mario);
+            Assert.AreEqual(_mario.Coins, 1);
+        }
+    }
+}
--- SuperMarioClone/UnitTest/MockClasses/TestContentManager.cs
+++ SuperMarioClone/UnitTest/MockClasses/TestContentManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,12 @@ namespace UnitTest.MockClasses
 
         public override T Load<T>(string assetName)
         {
-            T result = default(T);
-            return result;
+            object result = new Texture2D(new GraphicsDevice(new GraphicsAdapter(), new GraphicsProfile(), new PresentationParameters()), 200, 200); //TODO: FIXME, Make it actually return a working mock sprite.
+            if (result is T)
+            {
+                return (T)result;
+            }
+            return default(T);
         }
     }
 }
