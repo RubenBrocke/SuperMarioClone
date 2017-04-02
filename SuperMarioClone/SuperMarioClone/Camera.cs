18ba8cd94d99a5e6da2d56089d14fe3335a4a6c4
PrincePanda <ruben@marienhove.nl>
2017-04-02 18:02:00 +0200

    Modified camera to stick to the sides of a level

--- SuperMarioClone/SuperMarioClone/Camera.cs
+++ SuperMarioClone/SuperMarioClone/Camera.cs
@@ -16,6 +16,7 @@ namespace SuperMarioClone
         private float Rotation { get; set; }
         private float Zoom { get; set; }
         private Vector2 Origin { get; set; }
+        private CameraState _state;
 
         /// <summary>
         /// Constructor sets the values of different properties and variables
@@ -24,12 +25,14 @@ namespace SuperMarioClone
         public Camera(Viewport viewport)
         {
             _viewport = viewport;
-
             Position = Vector2.Zero;
             Rotation = 0;
             Zoom = 3.6f;
             Origin = new Vector2(this._viewport.Width / 2f, this._viewport.Height / 2f);
+            _state = CameraState.Follow;
         }
+
+        int test = 1;
 
         /// <summary>
         /// Centers camera on the given position
@@ -37,8 +40,54 @@ namespace SuperMarioClone
         /// <param name="_pos">Position camera should focus on</param>
         public void LookAt(Vector2 _pos)
         {
-            Position = new Vector2(Math.Max(-289, _pos.X - _viewport.Width / 2), -100); //Y = _pos.Y - 534 to follow player sort of
-            Origin = new Vector2(_viewport.Width / 2, _viewport.Height);
+            test = (int)Lerp(test, 100, 1);
+            Console.WriteLine(test);
+            //Switch states
+            switch (_state)
+            {
+                case CameraState.RightAlign:
+                    if (_pos.X - _viewport.Width / 2 + 50 > Global.Instance.MainGame.currentLevel.Width - 289)
+                    {
+                        _state = CameraState.Follow;
+                        Console.WriteLine("SWITCHED TO FOLLOW");
+                    }
+                    break;
+                case CameraState.LeftAlign:
+                    if (_pos.X - _viewport.Width / 2  - 50 > -289)
+                    {
+                        _state = CameraState.Follow;
+                        Console.WriteLine("SWITCHED TO FOLLOW");
+                    }
+                    break;
+                case CameraState.Follow:
+                    if (_pos.X - _viewport.Width / 2 < -289)
+                    {
+                        _state = CameraState.LeftAlign;
+                        Console.WriteLine("SWITCHED TO LEFT ALIGN");
+                    }
+                    else if (_pos.X - _viewport.Width / 2 > Global.Instance.MainGame.currentLevel.Width - 289)
+                    {
+                        _state = CameraState.RightAlign;
+                        Console.WriteLine("SWITCHED TO RIGHT ALIGN");
+                    }
+                    break;
+            }
+
+            switch (_state)
+            {
+                case CameraState.RightAlign:
+                    Position = new Vector2(Global.Instance.MainGame.currentLevel.Width - 289, -100);
+                    Origin = new Vector2(_viewport.Width / 2, _viewport.Height);
+                    break;
+                case CameraState.LeftAlign:
+                    Position = new Vector2(-289, -100);
+                    Origin = new Vector2(_viewport.Width / 2, _viewport.Height);
+                    break;
+                case CameraState.Follow:
+                    Position = new Vector2(_pos.X - _viewport.Width / 2, -100); //Y = _pos.Y - 534 to follow player sort of
+                    Origin = new Vector2(_viewport.Width / 2, _viewport.Height);
+                    break;               
+            }
         }
 
         /// <summary>
@@ -63,5 +112,17 @@ namespace SuperMarioClone
         {
             return new Rectangle((int)Position.X + 289, (int)Position.Y + 500, (int)(_viewport.Width / Zoom), (int)(_viewport.Height / Zoom));
         }
+
+        float Lerp(float firstFloat, float secondFloat, float by)
+        {
+            return firstFloat * by + secondFloat * (1 - by);
+        }
+    }
+
+    public enum CameraState
+    {
+        RightAlign,
+        LeftAlign,
+        Follow,
     }
 }
