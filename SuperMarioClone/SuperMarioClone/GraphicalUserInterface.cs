e6dc54bc1689a55c50e27bbb6122d8eeb7e9d95e
Danny de Jong <dannygjdejong@gmail.com>
2017-04-06 00:07:47 +0200

    Modified: GUI and Mario
    Modified:
    -Made GUI prettier
    -Mario now actually falls off screen when he dies

--- SuperMarioClone/SuperMarioClone/GraphicalUserInterface.cs
+++ SuperMarioClone/SuperMarioClone/GraphicalUserInterface.cs
@@ -13,23 +13,46 @@ namespace SuperMarioClone
     {
         private Mario _mario;
         private SpriteFont _font;
+        private Animator _coinAnimator;
+        private Texture2D _coinSheet;
 
-        public GraphicalUserInterface(Mario mario, ContentManager _contentManager)
+        public GraphicalUserInterface(Mario mario, ContentManager contentManager)
         {
             _mario = mario;
-            _font = _contentManager.Load<SpriteFont>("MarioFont");
+            _font = contentManager.Load<SpriteFont>("MarioFont");
+            _coinSheet = contentManager.Load<Texture2D>("CoinSheet");
+            _coinAnimator = new Animator(_coinSheet, 180);
+            _coinAnimator.GetTextures(0, 0, 16, 16, 4, 1);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-            spriteBatch.DrawString(_font, String.Format("Coins: {0,4}", _mario.Coins), new Vector2(704, 0), Color.Yellow);
-            spriteBatch.DrawString(_font, String.Format("Lives: {0,4}", _mario.Lives), new Vector2(558, 0), Color.Yellow);
-            spriteBatch.DrawString(_font, String.Format("Time \n {0,3}", Global.Instance.MainGame.currentLevel.Time), new Vector2(400, 0), Color.Yellow);
-            spriteBatch.DrawString(_font, String.Format("Lives: {0,4}", _mario.Lives), new Vector2(558, 0), Color.Yellow);
+            spriteBatch.Draw(texture: _coinAnimator.GetCurrentTexture(), position: new Vector2(720, 8));
+            DrawBorderedText(_font, spriteBatch, String.Format("x {0, 4}", _mario.Coins), Color.Black, Color.White, new Vector2(736, 8));
+            DrawBorderedText(_font, spriteBatch, String.Format("Time \n {0,3}", Global.Instance.MainGame.currentLevel.Time), Color.Black, Color.Yellow, new Vector2(375, 8));
+            DrawBorderedText(_font, spriteBatch, String.Format("Lives \n x {0,4}", _mario.Lives), Color.Black, Color.White, new Vector2(16, 8));
             spriteBatch.End();
         }
 
-
+        /// <summary>
+        /// Draws a given string with a border around it
+        /// </summary>
+        /// <param name="spriteBatch">SpriteBatch needed to draw the string</param>
+        /// <param name="text">The string that needs to be drawn</param>
+        /// <param name="borderColor">Color of the border</param>
+        /// <param name="textColor">Color of the text itself</param>
+        /// <param name="position">Position of the text on the screen</param>
+        /// <param name="scale">Scale of the text, this defaults to 1</param>
+        /// <param name="rotation">Rotation of the text, this defaults to 0</param>
+        private void DrawBorderedText(SpriteFont font, SpriteBatch spriteBatch, string text, Color borderColor, Color textColor, Vector2 position, float borderWidth = 1, float scale = 1f, float rotation = 0f)
+        {
+            Vector2 origin = Vector2.Zero;
+            spriteBatch.DrawString(font, text, new Vector2(position.X + borderWidth * scale, position.Y + borderWidth * scale), borderColor, rotation, origin, scale, SpriteEffects.None, 1f);
+            spriteBatch.DrawString(font, text, new Vector2(position.X + -borderWidth * scale, position.Y + -borderWidth * scale), borderColor, rotation, origin, scale, SpriteEffects.None, 1f);
+            spriteBatch.DrawString(font, text, new Vector2(position.X + -borderWidth * scale, position.Y + borderWidth * scale), borderColor, rotation, origin, scale, SpriteEffects.None, 1f);
+            spriteBatch.DrawString(font, text, new Vector2(position.X + borderWidth * scale, position.Y + -borderWidth * scale), borderColor, rotation, origin, scale, SpriteEffects.None, 1f);
+            spriteBatch.DrawString(font, text, position, textColor, rotation, origin, scale, SpriteEffects.None, 1f);
+        }
     }
 }
