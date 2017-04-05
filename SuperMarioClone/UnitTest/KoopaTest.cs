4b08a5ae4061c01007636b0ac4d4004d64486882
PrincePanda <ruben@marienhove.nl>
2017-04-06 01:21:09 +0200

    Added: more unit tests

---
+++ SuperMarioClone/UnitTest/KoopaTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarioClone;
+using System.Reflection;
+using UnitTest.MockClasses;
+using Microsoft.Xna.Framework;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class KoopaTest
+    {
+        private TestContentManager _contentManager;
+        private Level _level;
+        private Koopa _koopa;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _contentManager = new TestContentManager();
+            _level = new Level();
+            _koopa = new Koopa(0, 0, _level, _contentManager);
+            _level.ToAddGameObject(_koopa);
+            _level.UpdateLevel();
+        }
+
+        [TestMethod]
+        public void Koopa_UpdateHitbox()
+        {            
+            Rectangle testRect = new Rectangle((int)_koopa.Position.X, (int)_koopa.Position.Y, _koopa.Hitbox.Width, _koopa.Hitbox.Height);
+            testRect.Y += (int)_koopa.Gravity;
+            _level.UpdateLevel();
+            Assert.AreEqual(testRect, _koopa.Hitbox);
+        }
+
+        [TestMethod]
+        public void Koopa_AddGravity()
+        {
+            float test = _koopa.VelocityY + _koopa.Gravity;
+            _level.UpdateLevel();
+            Assert.AreEqual(test, _koopa.VelocityY);
+        }
+
+        [TestMethod]
+        public void Koopa_CollisionCheck()
+        {
+            Floor f = new Floor((int)_koopa.Position.X, (int)_koopa.Position.Y + _koopa.Hitbox.Height, 200, 200, _level, _contentManager);
+            Floor f2 = new Floor((int)_koopa.Position.X / 16 - 1, (int)_koopa.Position.Y / 16, _koopa.Hitbox.Width / 16, _koopa.Hitbox.Height / 16, _level, _contentManager);
+            _level.ToAddGameObject(f);
+            _level.ToAddGameObject(f2);
+            _level.UpdateLevel();
+            _level.UpdateLevel();
+            Assert.AreEqual(0.5, _koopa.VelocityX);
+        }
+
+        [TestMethod]
+        public void Koopa_UpdatePosition()
+        {
+            Vector2 testPos = new Vector2(_koopa.Position.X + 1f, 0.9f);
+            _level.UpdateLevel();
+            bool isTrue = ((int)testPos.X == (int)_koopa.Position.X && (int)testPos.Y == (int)_koopa.Position.Y);
+            Assert.IsTrue(isTrue);
+        }
+    }
+}
---
+++ SuperMarioClone/UnitTest/MuncherTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarioClone;
+using System.Reflection;
+using UnitTest.MockClasses;
+using Microsoft.Xna.Framework;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MunsherTest
+    {
+        private TestContentManager _contentManager;
+        private Level _level;
+        private Muncher _muncher;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _contentManager = new TestContentManager();
+            _level = new Level();
+            _muncher = new Muncher(1, 1, _level, _contentManager);
+        }
+
+        [TestMethod]
+        public void CloudBlock_CloudBlockk()
+        {
+            Assert.AreEqual(_muncher.Position, new Vector2(16, 16));
+            Assert.AreEqual(_muncher.Hitbox, new Rectangle(16, 16, 16, 16));
+            Assert.AreEqual(_muncher.CurrentLevel, _level);
+        }
+    }
+}
