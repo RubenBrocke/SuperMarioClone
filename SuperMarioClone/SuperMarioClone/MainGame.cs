f93c5345d8bd95cae6b97e3f325c0a2450433322
PrincePanda <ruben@marienhove.nl>
2017-04-06 23:35:00 +0200

    Added: Paralaxing background

--- SuperMarioClone/SuperMarioClone/MainGame.cs
+++ SuperMarioClone/SuperMarioClone/MainGame.cs
@@ -99,7 +99,8 @@ namespace SuperMarioClone
             {
                 GraphicsDevice.Clear(Color.CornflowerBlue);
                 _spriteBatch.Begin();
-                _spriteBatch.Draw(_background, new Rectangle(0, 0, 2000, 800), Color.White);
+                _spriteBatch.Draw(_background, new Rectangle(-(int)camera.Position.X - GraphicsDevice.Viewport.Width / 2, 0, 2000, 800), Color.White);
+                _spriteBatch.Draw(_background, new Rectangle(-(int)camera.Position.X - GraphicsDevice.Viewport.Width / 2 + 2000, 0, 2000, 800), Color.White);
                 _spriteBatch.End();
                 _spriteBatch.Begin(transformMatrix: camera.GetMatrix(), samplerState: SamplerState.PointClamp);
                 currentLevel.DrawLevel(_spriteBatch, GraphicsDevice.Viewport);
